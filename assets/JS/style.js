34c9fa72a4edb33d8ef97d5a2db239f929c688a0
unknown <ksuleymanli99@gmail.com>
2023-07-30 23:53:56 +0300

    JS PRACTIK #2

---
+++ assets/JS/style.js
@@ -0,0 +1,69 @@
+{let a=5
+let b=6
+console.log(a*b)}
+
+{let a=6
+let b=7
+a===5?   console.log("Ela") : console.log("Pis");}
+
+{
+    const a=6
+    const b=7
+    console.log(a)
+}
+{
+    const a=6
+    const b=7
+    console.log(b)
+}
+ {
+    let a=8
+    let b=7
+    a<=0.5? console.log("Kafi") : console.log("qeyri kafi")
+ }
+
+ {
+    let a=9
+    let b="F"
+    console.log( typeof typeof a)
+ }
+
+ {
+    let a=9
+    let b="F"
+    console.log(typeof b)
+ }
+
+ {
+    const a=6
+    const b=7
+    console.log(a*b)
+ }
+
+ {
+    const a=2
+    const b=9
+    console.log(a**b)
+ }
+ {
+    const a=5
+    const b="F"
+    console.log(a+b)
+ }
+
+ {
+    let a=4
+    let b=5
+    console.log(b%a)
+ }
+ {
+    let a=4
+    let b=5
+    console.log(a%b)
+ }
+
+ {
+    let a=9
+    let b=3
+    console.log(a%b)
+ }
